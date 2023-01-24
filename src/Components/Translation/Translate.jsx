d718b6694c272ed68a57b73ef1c9f8c2fa3d9b94
SondreSequence <sondre.kristoffer.reinholdtsen@no.experis.com>
2023-01-24 14:01:47 +0100

    Chore: Style changes (Animation and centered Nav)

---
+++ src/Components/Translation/Translate.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setImageData, setTranslation } from "../Reducers/translationReducer";
+
+function Translate() {
+  const englishSignsToHandEmojis = {
+    A: require("./Individual_Signs/a.png"),
+    B: require("./Individual_Signs/b.png"),
+    C: require("./Individual_Signs/c.png"),
+    D: require("./Individual_Signs/d.png"),
+    E: require("./Individual_Signs/e.png"),
+    F: require("./Individual_Signs/f.png"),
+    G: require("./Individual_Signs/g.png"),
+    H: require("./Individual_Signs/h.png"),
+    I: require("./Individual_Signs/i.png"),
+    J: require("./Individual_Signs/j.png"),
+    K: require("./Individual_Signs/k.png"),
+    L: require("./Individual_Signs/l.png"),
+    M: require("./Individual_Signs/m.png"),
+    N: require("./Individual_Signs/n.png"),
+    O: require("./Individual_Signs/o.png"),
+    P: require("./Individual_Signs/p.png"),
+    Q: require("./Individual_Signs/q.png"),
+    R: require("./Individual_Signs/r.png"),
+    S: require("./Individual_Signs/s.png"),
+    T: require("./Individual_Signs/t.png"),
+    U: require("./Individual_Signs/u.png"),
+    V: require("./Individual_Signs/v.png"),
+    W: require("./Individual_Signs/w.png"),
+    X: require("./Individual_Signs/x.png"),
+    Y: require("./Individual_Signs/y.png"),
+    Z: require("./Individual_Signs/z.png"),
+  };
+
+  function mapEnglishSignsToHandEmojis(signs) {
+    const imageSource = [];
+    for (const sign of signs) {
+      if (englishSignsToHandEmojis[sign]) {
+        imageSource.push(englishSignsToHandEmojis[sign]);
+      }
+    }
+
+    dispatch(setImageData(imageSource));
+  }
+
+  const output = useSelector((state) => state.translation.currentTranslation);
+  const imageSource = useSelector((state) => state.translation.imageData);
+  const dispatch = useDispatch();
+
+  const handleOnChange = (event) => {
+    dispatch(setTranslation(event.target.value));
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    mapEnglishSignsToHandEmojis(output.toUpperCase());
+  };
+
+  function generateImages() {
+    const images = imageSource.map((image) => <img className="animate__animated animate__flip animate__delay-0s" src={image}></img>);
+    return images;
+  }
+
+  const [textareaclassName, setTextAreaclassName] = useState(
+    "slowlyVisible form-control me-2 m-3 d-none"
+  );
+  return (
+    <nav className="flex-container navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <form
+          className="d-flex align-items-center mx-auto"
+          role="search"
+          onSubmit={handleOnSubmit}
+        >
+          <input
+            onChange={handleOnChange}
+            className="form-control me-2 m-3"
+            type="search"
+            placeholder="Translate Text"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success"
+            type="submit"
+            onClick={() =>
+              setTextAreaclassName(
+                "slowlyVisible form-control me-2 m-3 animate__animated animate__rollIn animate__delay-0s"
+              )
+            }
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <nav
+        contentEditable="true"
+        id="translateArea"
+        className="form-control mx-auto m-3"
+      >
+        {generateImages()}
+      </nav>
+    </nav>
+    
+  );
+}
+
+export default Translate;

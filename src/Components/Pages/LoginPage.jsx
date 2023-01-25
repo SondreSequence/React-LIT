5bf3d9a11dcf743a5351eb1e381b09b05e3a4dfa
Tartarpaste <tordvg@hotmail.com>
2023-01-25 09:07:42 +0100

    chore: more cleanup after merge

--- src/Components/Pages/LoginPage.jsx
+++ src/Components/Pages/LoginPage.jsx
@@ -1,13 +1,10 @@
 import LoginForm from "../Login/LoginForm";
-import MyComponent from "../TranslationHistory";
 
 function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
-      <MyComponent/>
       <LoginForm />
-
     </div>
   );
 }
--- src/Components/Translation/ImageGeneration.jsx
+++ src/Components/Translation/ImageGeneration.jsx
@@ -1,70 +1,74 @@
 import { useSelector, useDispatch } from "react-redux";
-import { setImageData } from "../Reducers/translationReducer"; 
-export { MapEnglishSignsToHandEmojis }
+import { setImageData } from "../Reducers/translationReducer";
+export { MapEnglishSignsToHandEmojis };
 
 const englishSignsToHandEmojis = {
-    A: require("./Individual_Signs/a.png"),
-    B: require("./Individual_Signs/b.png"),
-    C: require("./Individual_Signs/c.png"),
-    D: require("./Individual_Signs/d.png"),
-    E: require("./Individual_Signs/e.png"),
-    F: require("./Individual_Signs/f.png"),
-    G: require("./Individual_Signs/g.png"),
-    H: require("./Individual_Signs/h.png"),
-    I: require("./Individual_Signs/i.png"),
-    J: require("./Individual_Signs/j.png"),
-    K: require("./Individual_Signs/k.png"),
-    L: require("./Individual_Signs/l.png"),
-    M: require("./Individual_Signs/m.png"),
-    N: require("./Individual_Signs/n.png"),
-    O: require("./Individual_Signs/o.png"),
-    P: require("./Individual_Signs/p.png"),
-    Q: require("./Individual_Signs/q.png"),
-    R: require("./Individual_Signs/r.png"),
-    S: require("./Individual_Signs/s.png"),
-    T: require("./Individual_Signs/t.png"),
-    U: require("./Individual_Signs/u.png"),
-    V: require("./Individual_Signs/v.png"),
-    W: require("./Individual_Signs/w.png"),
-    X: require("./Individual_Signs/x.png"),
-    Y: require("./Individual_Signs/y.png"),
-    Z: require("./Individual_Signs/z.png"),
-  };
+  A: require("./Individual_Signs/a.png"),
+  B: require("./Individual_Signs/b.png"),
+  C: require("./Individual_Signs/c.png"),
+  D: require("./Individual_Signs/d.png"),
+  E: require("./Individual_Signs/e.png"),
+  F: require("./Individual_Signs/f.png"),
+  G: require("./Individual_Signs/g.png"),
+  H: require("./Individual_Signs/h.png"),
+  I: require("./Individual_Signs/i.png"),
+  J: require("./Individual_Signs/j.png"),
+  K: require("./Individual_Signs/k.png"),
+  L: require("./Individual_Signs/l.png"),
+  M: require("./Individual_Signs/m.png"),
+  N: require("./Individual_Signs/n.png"),
+  O: require("./Individual_Signs/o.png"),
+  P: require("./Individual_Signs/p.png"),
+  Q: require("./Individual_Signs/q.png"),
+  R: require("./Individual_Signs/r.png"),
+  S: require("./Individual_Signs/s.png"),
+  T: require("./Individual_Signs/t.png"),
+  U: require("./Individual_Signs/u.png"),
+  V: require("./Individual_Signs/v.png"),
+  W: require("./Individual_Signs/w.png"),
+  X: require("./Individual_Signs/x.png"),
+  Y: require("./Individual_Signs/y.png"),
+  Z: require("./Individual_Signs/z.png"),
+};
 
-  
-  function MapEnglishSignsToHandEmojis(signs, dispatch) {
-    const imageSource = [];
-    for (const sign of signs) {
-      if (englishSignsToHandEmojis[sign]) {
-        imageSource.push(englishSignsToHandEmojis[sign]);
-      }
+function MapEnglishSignsToHandEmojis(signs, dispatch) {
+  const imageSource = [];
+  for (const sign of signs) {
+    if (englishSignsToHandEmojis[sign]) {
+      imageSource.push(englishSignsToHandEmojis[sign]);
     }
-
-    dispatch(setImageData(imageSource));
   }
 
+  dispatch(setImageData(imageSource));
+}
 
-export default function ImageGeneration(){
-  
-    const imageSource = useSelector((state) => state.translation.imageData);
+export default function ImageGeneration() {
+  const imageSource = useSelector((state) => state.translation.imageData);
 
   function randomizer(min, max) {
     let previousNumbers = [];
     let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     while (previousNumbers.includes(randomNumber)) {
-        randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     }
     previousNumbers.push(randomNumber);
     return randomNumber;
-}
+  }
 
   function generateImages() {
-    const images = imageSource.map((image) => <img key={randomizer(1, 1000)} className="animate__animated animate__flip animate__delay-0s" src={image}></img>);
+    const images = imageSource.map((image) => (
+      <img
+        key={randomizer(1, 1000)}
+        className="animate__animated animate__flip animate__delay-0s"
+        src={image}
+      ></img>
+    ));
     return images;
   }
 
-return (<div
-    id="translateArea"
-    className="form-control mx-auto m-3">
-    {generateImages()}
-  </div>)}
+  return (
+    <div id="translateArea" className="form-control mx-auto m-3">
+      {generateImages()}
+    </div>
+  );
+}

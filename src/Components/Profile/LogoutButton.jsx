ffeedaeaa7c38b37d8d286ae506194fba2b85570
Tartarpaste <tordvg@hotmail.com>
2023-01-25 17:46:43 +0100

    feat: login logic is now up to spec.

--- src/Components/Profile/LogoutButton.jsx
+++ src/Components/Profile/LogoutButton.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
+/* 
+This function logs a user out of the webpage by deleting local storage. The button also boots the user to the login page  
+*/
+
 function LogoutButton() {
   function handleClearLocalStorage() {
     localStorage.clear();
